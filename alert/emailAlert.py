491063a1b4cca47f2cf151982bb6645af5d7423f
Lu Wang <luwang@Lus-MacBook-Air.local>
2023-04-01 11:01:23 -0500

    email alter with image

--- alert/emailAlert.py
+++ alert/emailAlert.py
@@ -1,9 +1,6 @@
-import datetime
-import os
+import os, base64
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
+from email.message import EmailMessage
 
 class EmailAlert:
     def __init__(self, title, from_email, to_email) -> None:
@@ -12,11 +9,14 @@ class EmailAlert:
         self.toemail = to_email
 
     def sendAlert(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+
         # Add an image to the message object (optional)
-        image = "<img src=\"meow.png.jpg\" alt=\"My Image\">"
+        with open(os.path.join(current_dir, "what.png"), "rb") as image_file:
+            image = base64.b64encode(image_file.read()).decode("utf-8")
+
 
         #read in css and html
-        current_dir = os.path.dirname(os.path.realpath(__file__))
         with open(os.path.join(current_dir, "emailCSS.txt")) as css:
             rawcss = css.read()
         with open(os.path.join(current_dir, "htmlMsg.txt")) as html:
@@ -25,18 +25,12 @@ class EmailAlert:
         # Create the HTML content
         html_content = rawhtml.format(self.subject, rawcss, "There", "This is a test", image)
 
-        # Add the HTML content to the message object
-        html_part = MIMEText(html_content, "html")
-        message.attach(html_part)
-
-
-
         # Connect to the SMTP server and send the email
         smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
         smtp_server.starttls()
-        smtp_server.login("myemail", "mypwd")
+        smtp_server.login("luwang827@gmail.com", "kqrfbyrsprgfoiuu")
         smtp_server.sendmail(self.fromemail, self.toemail, html_content)
         smtp_server.quit()
 
-alert = EmailAlert("Yoyo Check it out","myemail","youremail")
+alert = EmailAlert("Yoyo Check it out","luwang827@gmail.com","luwang827@gmail.com")
 alert.sendAlert()
